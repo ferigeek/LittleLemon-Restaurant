71035520a85d53890fb79e29b9100417b064d801
ferigeek <f.sarmeili@yahoo.com>
2024-09-28 20:43:03 +0330

    home.html

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
-    
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
+    path('home/', views.home, name='home'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'page_title': 'Home', 'host': request.get_host()})
